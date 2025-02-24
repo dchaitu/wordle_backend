6bd4e7937259dde4869436f983e60c550dadce8d
dchaitu <chaitanyadokara@gmail.com>
2025-02-24 22:56:09 +0530

    updated code to a Docker file

--- wordle_backend/settings.py
+++ wordle_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j$+a4o#h_*-b%a^1o8v+o=1oerg9*odin-u$lbad3yclfw&+f7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'wordle_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('DATABASE_NAME','wordle-db'),
+        'USER': os.environ.get('DATABASE_USER','admin'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD','admin'),
+        'HOST': os.environ.get('DATABASE_HOST','localhost'),
+        'PORT': '3306',
     }
 }
 
